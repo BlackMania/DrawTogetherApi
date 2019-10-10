15e0bc5fb0d1f2b172f8eea684c17e9e131f2648
Vincent <vincentmuijtjens@ziggo.nl>
2019-10-10 14:42:42 +0200

    Registered filters and resources for the API

--- src/main/java/com/api/APIApp.java
+++ src/main/java/com/api/APIApp.java
@@ -1,9 +1,17 @@
 package com.api;
 
+import com.api.logincomponent.filters.BasicAuthFilter;
+import com.api.logincomponent.filters.TokenAuthFilter;
+import com.api.logincomponent.resources.LoginResource;
 import org.glassfish.jersey.server.ResourceConfig;
 
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("api")
 public class APIApp extends ResourceConfig {
     public APIApp() {
-
+        register(LoginResource.class);
+        register(BasicAuthFilter.class);
+        register(TokenAuthFilter.class);
     }
 }
