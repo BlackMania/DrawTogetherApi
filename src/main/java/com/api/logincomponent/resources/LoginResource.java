58135f20ccadd9f878ec1d7da0abc93590c892e3
BlackMania <BlackMania@users.noreply.github.com>
2019-10-11 18:58:35 +0200

    Added resources to be called to verify login

---
+++ src/main/java/com/api/logincomponent/resources/LoginResource.java
@@ -0,0 +1,37 @@
+package com.api.logincomponent.resources;
+
+import com.api.logincomponent.tokenlogic.TokenHelper;
+import com.sun.jersey.core.util.Base64;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.*;
+import java.util.StringTokenizer;
+
+@Path("secured")
+public class LoginResource {
+
+    @GET
+    @Path("login")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String login(@Context HttpHeaders headers){
+        TokenHelper tokenHelper = new TokenHelper();
+        String userInfo = headers.getRequestHeader("Authorization").get(0);
+        userInfo = userInfo.replace("Basic ", "");
+        String decodedString = Base64.base64Decode(userInfo);
+        StringTokenizer tokenizer = new StringTokenizer(decodedString, ":");
+        String username = tokenizer.nextToken();
+        return tokenHelper.issueToken(username);
+    }
+
+    @GET
+    @Path("tokenauth")
+    public Response verify() {
+        return Response
+                .status(Response.Status.OK)
+                .entity(Response.Status.OK.getStatusCode() + " " + Response.Status.OK.toString())
+                .build();
+    }
+}
+
