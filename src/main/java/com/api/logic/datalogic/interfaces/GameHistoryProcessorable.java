923626d62e06efea5afaff12e08d2a6bb20f4a21
BlackMania <BlackMania@users.noreply.github.com>
2019-12-24 11:46:11 +0100

    Quick fix in interface

--- src/main/java/com/api/logic/datalogic/interfaces/GameHistoryProcessorable.java
+++ src/main/java/com/api/logic/datalogic/interfaces/GameHistoryProcessorable.java
@@ -2,5 +2,8 @@ package com.api.logic.datalogic.interfaces;
 
 import com.api.dalcomponent.model.Game;
 
+import java.util.List;
+
 public interface GameHistoryProcessorable<T extends Game> extends Processerable<Game> {
+    List<Game> getByClientId(String clientid);
 }
