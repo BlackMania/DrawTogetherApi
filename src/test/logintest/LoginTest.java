8222d37a490b38a4b377a8e9ab5c79b0edde3e89
Vincent <vincentmuijtjens@ziggo.nl>
2019-12-26 13:26:23 +0100

    Added comments how to test private classes

--- src/test/logintest/LoginTest.java
+++ src/test/logintest/LoginTest.java
@@ -1,24 +1,13 @@
 package logintest;
 
-import com.api.logic.loginlogic.PasswordHasher;
-import org.junit.Assert;
-import org.junit.Test;
-
 public class LoginTest {
+
+    // TEST CLASSES AND METHODS WHO ARE PRIVATE:
+    // Classes:
+    //      @Jailbreak Foo foo = new Foo();
 
     private String password;
 
     public LoginTest() {
-        PasswordHasher hasher = new PasswordHasher();
-        password = hasher.generateHash("password");
-    }
-
-    @Test
-    public void Login()
-    {
-        PasswordHasher hasher = new PasswordHasher();
-        Boolean succes = hasher.validatePassword("password", password);
-
-        Assert.assertEquals(true, succes);
     }
 }
