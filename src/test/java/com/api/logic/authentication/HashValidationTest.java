5e160f26e43e9a8e535ef815634fa37d28ada54c
Vincent <vincentmuijtjens@ziggo.nl>
2019-12-26 14:55:43 +0100

    Added another test which validates that when a wrong iteration count is used, the hash is invalid.

--- src/test/java/com/api/logic/authentication/HashValidationTest.java
+++ src/test/java/com/api/logic/authentication/HashValidationTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
 
 public class HashValidationTest {
 
@@ -25,5 +26,25 @@ public class HashValidationTest {
         String storedHash = hasher.generateStrongPassword("Password1!");
 
         Assert.assertFalse(validator.validatePassword("anotherpassword", storedHash));
+    }
+
+    @Test
+    public void testPasswordGeneratedButWithAdjustedIteratorCount() throws InvalidKeySpecException, NoSuchAlgorithmException {
+        HashValidator validator = new HashValidator();
+        PasswordHasher hasher = new PasswordHasher();
+
+        String storedHash = hasher.generateStrongPassword("Password1!");
+
+        // Value is still same generated since the iteration count isn't changed.
+        Assert.assertTrue(validator.validatePassword("Password1!", storedHash));
+
+        String[] splitHash = storedHash.split(":");
+
+        splitHash[0] = "208234";
+
+        storedHash = splitHash[0] + ":" + splitHash[1] + ":" + splitHash[2];
+
+        // The iteration count is changed so the generated hash is not the same as the stored hash anymore. Since it took more or less iterations to generate the outcome of the hash.
+        Assert.assertFalse(validator.validatePassword("Password1!", storedHash));
     }
 }
