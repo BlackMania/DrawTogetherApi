91fbe6f8c0874d7f904accfeec937b2fc8326fdb
BlackMania <BlackMania@users.noreply.github.com>
2019-12-30 12:55:56 +0100

    Removed classes which could be directly accessed

--- src/main/java/com/api/logic/datalogic/interfaces/GameHistoryProcessorable.java
+++
@@ -1,9 +0,0 @@
-package com.api.logic.datalogic.interfaces;
-
-import com.api.dalcomponent.model.Game;
-
-import java.util.List;
-
-public interface GameHistoryProcessorable<T extends Game> extends Processerable<Game> {
-    List<Game> getByClientId(String clientid);
-}
