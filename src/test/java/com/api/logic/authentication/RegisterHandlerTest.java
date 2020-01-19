1769def3a3feae72dd4a9b3e521f317b9d0d8133
Vincent <vincentmuijtjens@ziggo.nl>
2020-01-19 12:51:04 +0100

    Fixed test to remove user created after test is done

--- src/test/java/com/api/logic/authentication/RegisterHandlerTest.java
+++ src/test/java/com/api/logic/authentication/RegisterHandlerTest.java
@@ -7,6 +7,8 @@ import com.api.dalcomponent.repository.UserRepository;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.List;
+
 public class RegisterHandlerTest {
     private IUserRepository<User> userRepo;
     public RegisterHandlerTest() {
@@ -21,10 +23,8 @@ public class RegisterHandlerTest {
 
         Assert.assertEquals(2, userRepo.getAll().size());
 
-        User user = userRepo.getUserByUsername("Vincent");
-
-        userRepo.delete(user);
-
+        List<User> users = userRepo.getAll();
+        userRepo.delete(users.get(1));
         Assert.assertEquals(1, userRepo.getAll().size());
     }
 
